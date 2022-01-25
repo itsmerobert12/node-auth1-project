6e621aa33d1aa733cbd399c653d9c4f16dab3cde
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-24 20:01:13 -0600

    usernamefree/exists working

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+const User = require('../users/users-model')
+
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +9,8 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  next()
 }
 
 /*
@@ -18,9 +21,19 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
+async function checkUsernameFree(req, res, next) {
+  try {
+    const user = await User.findBy({ username: req.body.username })
+    if (!user.length) 
+    next()
+    
+    else next({ "message": 'Username taken' })
+  } catch (err) {
+    next(err)
+  }
+  }
+  
 
-}
 
 /*
   If the username in req.body does NOT exist in the database
@@ -30,8 +43,8 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  next()
 }
 
 /*
@@ -42,8 +55,14 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  next()
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
